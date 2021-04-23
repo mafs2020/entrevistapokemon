fb6d9352873ce6a75aef7a9465f317a229215454
MAFS <martin117h@gmail.com>
2021-04-23 18:23:31 -0500

    rutas funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// rutas padre
+import { rutasPadreModule } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    rutasPadreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
