c62efa6d482eee0fe167a6ede4b4a79467ba0719
MAFS <martin117h@gmail.com>
2021-04-27 19:19:15 -0500

    se incluye el servidor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   url: 'https://pokeapi.co/api/v2/pokemon',
 
   publicKey: "BD25VIEh-70Er4eyXgi_XgqlOK9ExYd7-MNocVn4ZWVFRSl5koKERdnDlVOCuQ0C4TtcmCODwDzbagRchdgY9U4",
-  server: "http://localhost:3000",
+  server: "https://entrevistapokemon.herokuapp.com/",
 };
 
 /*
